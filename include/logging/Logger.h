39269176468aae352ee541f99861aedc96ec1bcb
alex <alex.kollarik1@studen.kuleuven.be>
2025-07-11 21:32:18 +0200

    feat: Add a function to read information from file

--- include/logging/Logger.h
+++ include/logging/Logger.h
@@ -1,13 +1,11 @@
-//
-// Created by Alex on 11/07/2025.
-//
-
 #ifndef LOGGER_H
 #define LOGGER_H
-
+#include <string>
 
 
 class Logger {
+  public:
+  void line_getter(const std::string& filename);
 };
 
 
--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,7 +1,26 @@
-//
-// Created by Alex on 11/07/2025.
-//
-#include "Logger.h"
+#include "../include/logging/Logger.h"
+#include <fstream>
 #include <iostream>
+//#include <json/json.h>
+#include <string>
+
+void Logger::line_getter(const std::string& FILENAME) {
+  std::ifstream myfile (FILENAME); // instance of ifstream object
+  if (myfile.is_open()) {
+    std::string line;
+    while (std::getline (myfile, line)) {
+      std::cout << line << "\n";
+    }
+    myfile.close();
+  }
+  else {
+    std::cout << "Unable to open file ";
+  }
+}
+
+void json_parser(const std::string& DATA) { //using const std::string& so the data doesn't get copied but the incoming version gets changed into a json values
+
+
+}
 
 
