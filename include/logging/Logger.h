859945af69b19751bf8a95e2cc28bbe7e99100c9
alex <alex.kollarik@student.kuleuven.be>
2025-07-11 18:24:06 +0200

    initial logger branch set up

---
+++ include/logging/Logger.h
@@ -0,0 +1,15 @@
+//
+// Created by Alex on 11/07/2025.
+//
+
+#ifndef LOGGER_H
+#define LOGGER_H
+
+
+
+class Logger {
+};
+
+
+
+#endif //LOGGER_H
---
+++ src/Logger.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Alex on 11/07/2025.
+//
+#include "Logger.h"
+#include <iostream>
+
+
