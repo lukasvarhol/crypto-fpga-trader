16ef1c5bf6b5390253c6608237c7a777f49026e0
alex <alex.kollarik@student.kuleuven.be>
2025-07-12 14:53:00 +0200

    chore(Logger): removing irrelevant code

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -7,11 +7,9 @@
 
 void Logger::log_data(std::string type, std::string data, std::string message) {
   time_t timestamp; // seconds since 1970
-  time(&timestamp);
   struct tm* timeinfo = localtime(&timestamp);
   char filename[100];
   strftime(filename, sizeof(filename), "%Y-%m-%d.log", timeinfo);
-  std::cout << ctime(&timestamp);
   std::ofstream myfile (type+"-"+filename, std::ios::app);
   if (myfile.is_open()) {
     myfile << type << "," << data << ","<< message <<"\n";
